4fcbb125a60453746414eed491cc755cc48eaaf7
inaridiy <inainainariinainari@gmail.com>
2022-04-15 00:13:32 +0900

    yarn workspace対応

--- projects/app/src/pages/_app.tsx
+++ projects/app/src/pages/_app.tsx
@@ -1,13 +1,8 @@
-import { Web3Provider } from "@/components/Web3Provider";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Web3Provider>
-      <Component {...pageProps} />
-    </Web3Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
